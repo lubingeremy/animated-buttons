06018b116b79919937ad457a120f6428737eb551
Gérémy Lubin <97472153+lubingeremy@users.noreply.github.com>
2022-03-04 11:45:11 -0400

    Switch button successful

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let switchButton = document.getElementById("btn4")
 
-let switchHov = switchButton.parentElement.querySelector(':hover');
+// let switchHov = switchButton.parentElement.querySelector(':hover');
 // let style = window.getComputedStyle(switchButton, ':hover::before');
 // let transform = style.getPropertyValue('transform');
 
-console.log(switchHov)
+// console.log(switchHov)
 // console.log(style)
 // console.log(transform)
+
+
+const switchButton = document.getElementById("btn4")
+var stateButton = 0;
+
+switchButton.addEventListener('click', function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('sup');
+
+    if(stateButton === 0){
+        switchButton.style.setProperty("--posit", "translate(-0.255rem, 1.17rem)");
+        stateButton = 1;
+    } else{
+        switchButton.style.setProperty("--posit", "translate(-0.255rem, -0.019rem)");
+        stateButton = 0;
+    }
+    
+})
